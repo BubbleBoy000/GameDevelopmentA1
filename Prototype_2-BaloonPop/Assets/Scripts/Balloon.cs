8c2ab7a9e3d3647aec2ca7bb0dae863e862dea48
BubbleBoy000 <ethan.f610@stu.nebo.edu>
2025-09-10 09:29:57 -0600

    Created Balloon Scripts & BG/UI

---
+++ Prototype_2-BaloonPop/Assets/Scripts/Balloon.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Balloon : MonoBehaviour
+{
+    public int clicksToPop = 3; // Number of clicks required to pop the balloon
+    public float scaleToIncrease = 0.15f; // Amount to increase the scale on each click
+    public int scoreToGive; // Score to give when the balloon is popped
+    private ScoreManager scoreManager; // Reference to the ScoreManager script
+    public GameObject popEffect; // Particle effect prefab to instantiate on pop
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Updates every click
+    void OnMouseDown()
+    {
+        //Reduce clicks by one
+        clicksToPop -= 1;
+        //increase the size of the balloon
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        //Check if the balloon should pop
+        if (clicksToPop == 0)
+        {
+            //Add to the score
+            //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+            //scoreManager.UpdateScore(scoreToGive);
+
+            //Instantiate the pop effect
+            //Instantiate(popEffect, transform.position, transform.rotation);
+
+            //Destroy the balloon
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype_2-BaloonPop/Assets/Scripts/BalloonUp.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalloonUp : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * speed * Time.deltaTime); // Move the balloon upwards
+    }
+}
